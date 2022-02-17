746a2b2acbccb0889a7b6aefe41825ae450c81c5
Juan <jarobyte91@gmail.com>
2022-02-17 01:25:04 -0400

    first prototype

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from io import BytesIO
 import base64
 from nltk.tokenize import PunktSentenceTokenizer
 import json
+import pandas as pd
 
 server = Flask(__name__)
 app = Dash(
@@ -15,9 +16,11 @@ app = Dash(
     external_stylesheets = [dbc.themes.BOOTSTRAP],
     server = server
 )
+
 ###################################
 # Upload tab
 ###################################
+
 filename_table = html.Table(
     html.Tr(
         [
@@ -40,19 +43,8 @@ tab_upload = dbc.Tab(
 ###################################
 # Highlights tab
 ###################################
+
 query_test = """In this paper, we propose a novel neural network model called RNN Encoder-Decoder that consists of two recurrent neural networks (RNN). One RNN encodes a sequence of symbols into a fixed-length vector representation, and the other decodes the representation into another sequence of symbols. The encoder and decoder of the proposed model are jointly trained to maximize the conditional probability of a target sequence given a source sequence. The performance of a statistical machine translation system is empirically found to improve by using the conditional probabilities of phrase pairs computed by the RNN Encoder-Decoder as an additional feature in the existing log-linear model. Qualitatively, we show that the proposed model learns a semantically and syntactically meaningful representation of linguistic phrases."""
-# highlights_table = dbc.Table(
-#     [
-#         html.Tr(
-#             [
-#                 html.Th("Sentence"),
-#                 html.Th("Text"),
-#                 html.Th("Relevant?", colSpan = 2),
-#             ]
-#         ),
-#         html.Div(id = "highlight_rows")
-#     ]
-# )
 
 tab_highlights = dbc.Tab(
     label = "Highlights",
@@ -62,55 +54,59 @@ tab_highlights = dbc.Tab(
                 html.H3("Query"),
                 dbc.Textarea(id = "query", value = query_test, rows = 5),
             ],
-           #style = {"height":"50%", "overflowY":"scroll"}
            style = {"position":"sticky", "top":0}
         ),
         html.Div(
             [
                 html.H3("Sentences"),
                 html.Div(id = "highlights_table", 
-                    #style = {
-                    #    #"height":"50%", 
-                    #    #"position":"fixed",
-                    #    # "display":"flex",
-                    #    # "flex-flow":"column",
-                    #    # "flex":"1 1 auto",
-                    #    "overflowY":"scroll"
-                    #}
                 )
            ],
-           #style = {"height":"20%"}
         )
     ],
-    #style = {"height":"100%"}
 )
+
 ###################################
 # Summary tab
 ###################################
+
 tab_summary = dbc.Tab(
     label = "Summary",
     children = [
-        html.H3("Query"),
+        dbc.Row(
+            [
+                dbc.Col(html.H3("Query")),
+                dbc.Col(),
+                dbc.Col(dbc.Card(dbc.Button("Download .txt", id = "download_txt_button")), width = 2),
+                dbc.Col(dbc.Card(dbc.Button("Download .csv", id = "download_csv_button")), width = 2),
+            ]
+        ),
         html.P(id = "summary_query"),
-        html.H3("Accepted Sentences"),
-        html.Ul(id = "accepted_sentences")
+        html.H3("Selected Sentences"),
+        html.Div(id = "accepted_sentences")
     ]
 )
+
 ###################################
 # Main Container
 ###################################
+
 app.layout = dbc.Container(
     [
         dcc.Store(id = "sentences"),
         dcc.Store(id = "relevant"),
-        html.H1("FLAVIA - Baseline"),
+        dcc.Download(id = "download_txt"),
+        dcc.Download(id = "download_csv"),
+        html.H1("Baseline"),
         dbc.Tabs([tab_upload, tab_highlights, tab_summary])
     ],
     fluid = True
 )
+
 ###################################
 # Callbacks
 ###################################
+
 @app.callback(
     Output("summary_query", "children"),
     Input("query", "value")
@@ -169,7 +165,6 @@ def update_highlights(sentences):
            [
                html.Th("Sentence"),
                html.Th("Text"),
-               # html.Th("Relevant?", colSpan = 2),
            ]
         )
         output = [header]
@@ -178,111 +173,136 @@ def update_highlights(sentences):
                 [
                     html.Td(i), 
                     html.Td(s), 
-                    # html.Td(dbc.Button(
-                    #         "Y", 
-                    #         id = dict(kind = "accept", index = i),
-                    #         style = {"background":"seagreen"}
-                    #     )
-                    # ), 
-                    # html.Td(dbc.Button(
-                    #         "N", 
-                    #         id = dict(kind = "reject", index = i),
-                    #         style = {"background":"firebrick"}
-                    #     )
-                    # ), 
                 ],
                 id = dict(kind = "highlight_row", index = i)
             )
             output.append(row)
-        #return dbc.Table(output, style = {"height":"300px", "overflowY":"scroll"})
         return dbc.Table(output)
     else:
-        return None
-
-# @app.callback(
-#     Output("relevant", "data"),
-#     Input({"kind":"accept", "index":MATCH}, "n_clicks"),
-#     State({"kind":"accept", "index":MATCH}, "index"),
-#     # Input({"kind":"reject", "index":ALL}, "n_clicks"),
-#     # Input("sentences", "data"),
-#     # State("sentences", "data"),
-# )
-# def update_relevant(*args):
-#     print("update relevant")
-#     ctx = dash.callback_context
-#     print(len(ctx.triggered))
-#     if len(ctx.triggered) > 1:
-#         return None
-#     prop_id = ctx.triggered[0]["prop_id"]
-#     value = ctx.triggered[0]["value"]
-#     print("prop_id", prop_id)
-#     print("value", type(value), value)
-#     if value is None:
-#         return None
-#     else:
-#         return None
-        # index_kind, attribute = prop_id.split(".")
-        # index_kind = json.loads(index_kind)
-        # index = index_kind["index"]
-        # kind = index_kind["kind"]
-        # print("index", index)
-        # print("kind", kind)
-
-#     new_sentences = args[-2]
-#     sentences = args[-1]
-#     if sentences:
-# #        sentences = json.loads(sentences)
-#         return [None for s in sentences]
-    # if not past_sentences:
-    #     return ""
-    # else:
-    #     #past_sentences = json.loads(past_sentences)
-    #     sentences = json.loads(sentences)
-    #     relevant = [None for s in sentences]
-    #     ctx = dash.callback_context
-    #     print(ctx.triggered)
-    #     prop_id = ctx.triggered[0]["prop_id"]
-    #     value = ctx.triggered[0]["value"]
-    #     if prop_id != "sentences" and value is not None:
-    #         index_type, attribute = prop_id.split(".")
-    #         index_type = json.loads(index_type)
-    #         index = index_type["index"]
-    #         type = index_type["type"]
-    #         if type == "accept":
-    #             relevant[index] = True
-    #         elif type == "reject":
-    #             relevant[index] = False 
-    #     return json.dumps(relevant)
-
-
-# @app.callback(
-#     Output({"type":"highlight_row", "index":ALL}, "style"),
-#     Input("relevant", "data")
-# )
-# def update_row_colors(relevant):
-#     if relevant:
-#         relevant = json.loads(relevant)
-#         output = []
-#         for r in relevant:
-#             style = {"background":"white"}
-#             if r:
-#                 style["background"] = "lightgreen"
-#             elif not r:
-#                 style["background"] = "lightpink"
-#             output.append(style)
-#         return output
-#     else:
-#         return None
+        return [html.Td("The sentences of the paper will appear here", 
+            id = dict(kind = "highlight_row", index = 0))]
 
 @app.callback(
     Output({"kind":"highlight_row", "index":MATCH}, "style"),
     Input({"kind":"highlight_row", "index":MATCH}, "n_clicks"),
+    prevent_initial_call = True
 )
 def update_row_colors(clicks):
-    print(dash.callback_context.triggered)
+    ctx = dash.callback_context.triggered[0]
+    prop_id = json.loads(ctx["prop_id"].split(".")[0])
+    index = prop_id["index"]
+    # print(ctx)
+    # print(index)
     if clicks:
         return {"background":"lightgreen"} if (clicks % 2) == 1 else {"background":"lightpink"}
 
+@app.callback(
+    Output("relevant", "data"),
+    Input({"kind":"highlight_row", "index":ALL}, "style"),
+    Input("sentences", "data"),
+    State("relevant", "data"),
+    # prevent_initial_call = True
+)
+def update_relevant(*args):
+    # print("\nupdate_relevant")
+    ctx = dash.callback_context.triggered[0]
+    #print(ctx)
+    prop_id = ctx["prop_id"]
+    ctx_value = ctx["value"]
+    # print(prop_id)
+    # print(ctx_value)
+    # print(ctx_value[:8])
+    # print(ctx_value[:10])
+    output = None
+    if (prop_id == "sentences.data" and args[-2]):
+        #print("loading sentences")
+        sentences = json.loads(args[-2])
+        output = json.dumps([None for s in sentences])
+    elif prop_id[:8] == '{"index"' and ctx_value and args[-1]:
+        #print("updating relevant")
+        index = json.loads(prop_id.split(".")[0])["index"]
+        relevant = json.loads(args[-1])
+        if ctx_value["background"] == "lightgreen":
+            relevant[index] = True
+        elif ctx_value["background"] == "lightpink":
+            relevant[index] = False
+        output = json.dumps(relevant)
+    elif prop_id == '{"index":0,"kind":"highlight_row"}.style' and ctx_value is None and args[-1]:
+        #print("initial table")
+        relevant = json.loads(args[-1])
+        output = json.dumps(relevant)
+    return output
+    # prop_id = json.loads(ctx["prop_id"].split(".")[0])
+    # index = prop_id["index"]
+    # print(prop_id)
+    # styles = args[:-1][0]
+    # print(styles)
+    # sentences = args[-1]
+    # # print(sentences)
+    # # print(len(sentences))
+    # if sentences:
+    #     sentences = json.loads(sentences)
+    #     # print(sentences)
+    #     print(len(sentences))
+    #     output = []
+    #     for s in styles:
+    #         output.append(None)
+    #     return json.dumps(output)
+    # else:
+    #     return None
+
+@app.callback(
+    Output("accepted_sentences", "children"),
+    Input("relevant", "data"),
+    State("sentences", "data")
+)
+def update_summary_table(relevant, sentences):
+    # print("\nupdate_summary_table")
+    # print(relevant)
+    if relevant:
+        relevant = json.loads(relevant)
+        sentences  = json.loads(sentences)
+        #print(relevant)
+        #return html.Ul([html.Li(f"{i}  -  {s}") for i, (s, r) in enumerate(zip(sentences, relevant)) if r])
+        return dbc.Table([html.Tr([html.Td(i), html.Td(s)]) for i, (s, r) in enumerate(zip(sentences, relevant)) if r])
+    else:
+        return None
+
+@app.callback(
+    Output("download_txt", "data"),
+    Input("download_txt_button", "n_clicks"),
+    State("sentences", "data"),
+    State("relevant", "data"),
+    prevent_initial_call = True
+)
+def download_txt(clicks, sentences, relevant):
+    filename = "summary.txt"
+    content = ""
+    if sentences and relevant:
+        sentences = json.loads(sentences)
+        relevant = json.loads(relevant)
+        content = "\n\n".join([s for s, r in zip(sentences, relevant) if r])
+    return dict(filename = filename, content = content)
+
+@app.callback(
+    Output("download_csv", "data"),
+    Input("download_csv_button", "n_clicks"),
+    State("sentences", "data"),
+    State("relevant", "data"),
+    prevent_initial_call = True
+)
+def download_csv(clicks, sentences, relevant):
+    filename = "summary.csv"
+    content = ""
+    if sentences and relevant:
+        sentences = json.loads(sentences)
+        relevant = json.loads(relevant)
+        content = pd.DataFrame(
+            [(i, s, r) for i, (s, r) in enumerate(zip(sentences, relevant)) if r is not None],
+            columns = ["sentence", "text", "relevant"]
+        ).to_csv()
+    return dict(filename = filename, content = content)
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port = 8040)
